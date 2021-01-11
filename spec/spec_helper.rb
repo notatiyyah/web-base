290e4658451a57dec3bec00c5195220a14139291
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-01-11 15:15:29 +0000

    Fix spec_helper naming bug

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,7 @@ require 'capybara/rspec'
 require 'rspec'
 
 # tell Capybara about our app class
-Capybara.app = BattleApp
+Capybara.app = App
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
